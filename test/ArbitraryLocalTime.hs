2982ea223632b69bc6f1256015a6898c618a2e09
Eugene <e@egeek.me>
2021-03-27 07:00:29 +0200

    Extract `Arbitrary LocalTime` implementation to a separate file

---
+++ test/ArbitraryLocalTime.hs
@@ -0,0 +1,24 @@
+module ArbitraryLocalTime where
+
+import Data.Fixed (Fixed(..))
+import Data.Time.Calendar
+import Data.Time.LocalTime
+import Test.QuickCheck
+
+-- This implements @Arbitrary@ for a valid @LocalTime@ and its parts directly.
+-- It's not clear why QuickCheck doesn't have this out of the box; maybe because
+-- there are multiple possible implementations and it's better to implement it
+-- on a more specific newtype?
+-- https://blog.ploeh.dk/2019/09/02/naming-newtypes-for-quickcheck-arbitraries/
+instance Arbitrary Day where
+  arbitrary = fmap ModifiedJulianDay arbitrary
+
+instance Arbitrary TimeOfDay where
+  arbitrary = do
+    hour <- choose (0, 23)
+    minute <- choose (0, 59)
+    second <- fmap MkFixed arbitrary
+    return $ TimeOfDay hour minute second
+
+instance Arbitrary LocalTime where
+  arbitrary = LocalTime <$> arbitrary <*> arbitrary
