3bfe8a6a422976cdfcff2e988bf19777a2ba6aa6
Eugene <e@egeek.me>
2021-09-27 21:25:46 +0300

    remove old commented implementation

--- src/GFS.hs
+++ src/GFS.hs
@@ -83,21 +83,5 @@ dropLast :: [a] -> [a]
 dropLast [] = []
 dropLast xs = take (length xs - 1) xs
 
---fromMaybe [] $ do
-  --let newestToOldest = reverse times
-  --(newest, others) <- uncons newestToOldest
-  --return . snd $ foldl' dropNextWhenCloseToPrevious (newest, []) others
-
-  --where
-    -- The folding function takes the oldest remaining time so far and if it's
-    -- closer than the @period@ to the next older time, that time is dropped
-    -- (appended to the state), otherwise it is made the oldest remaining time.
-    --dropNextWhenCloseToPrevious :: CleanupState -> LocalTime -> CleanupState
-    --dropNextWhenCloseToPrevious (oldest, dropped) older =
-      --let olderShouldBeDropped = False -- oldest `diffLocalTime` older <= period
-      --in if olderShouldBeDropped
-        --then (oldest, older : dropped)
-        --else (older, dropped)
-
 -- oldest remaining time so far and a list of dropped times
 type CleanupState = (LocalTime, [LocalTime])
