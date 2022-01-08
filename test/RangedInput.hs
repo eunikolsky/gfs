1266150a4aa86ad3ba3590b203dfd51b080fee42
Eugene <e@egeek.me>
2022-01-08 09:05:25 +0200

    Generate all, including newest, offsets for multiple periods

--- test/RangedInput.hs
+++ test/RangedInput.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module RangedInput where
@@ -112,7 +111,7 @@ type NewestTimes = Times
 -- |for the ease of writing tests.
 -- Sample output:
 -- `(2000-01-01 00:00:00,1999-12-31 23:59:59,(1 d,5 d),Sorted {getSorted = [1999-12-27 15:53:04,1999-12-28 00:50:13,1999-12-29 20:36:03,1999-12-30 14:32:27]},Sorted {getSorted = [1999-12-27 15:53:04,1999-12-27 16:53:56,1999-12-27 18:52:44,1999-12-27 19:24:03,1999-12-27 21:07:00,1999-12-27 22:50:25,1999-12-28 00:50:13,1999-12-28 00:55:15,1999-12-28 02:22:39,1999-12-28 08:52:45,1999-12-28 13:19:33,1999-12-29 20:36:03,1999-12-29 23:19:06,1999-12-30 14:32:27,1999-12-30 14:55:48,1999-12-30 17:32:40,1999-12-30 18:40:20,1999-12-30 19:25:54,1999-12-30 19:58:30,1999-12-30 21:17:13,1999-12-30 23:45:06]})`
-arbitraryInputWithinRangeSubperiods :: forall a. NumSubperiods a => Gen (BaseTestData Identity a)
+arbitraryInputWithinRangeSubperiods :: NumSubperiods a => Gen (BaseTestData Identity a)
 arbitraryInputWithinRangeSubperiods = arbitraryBaseTestData
   choose_
   $ \offsetFrom _ numSubperiods -> do
@@ -121,17 +120,16 @@ arbitraryInputWithinRangeSubperiods = arbitraryBaseTestData
 
       pure (concat offsets ++ coerce newestOffsets, newestOffsets)
 
-      where
-        generateOffsets :: Offset -> (a, a) -> Gen (NewestOffset, [Offset])
-        generateOffsets (Offset offsetFrom) (numSubperiodFrom, numSubperiodTo) = do
-          -- note: both offsets are shifted relative to `offsetFrom` in order not to start from `now`,
-          -- that's where the extra `+ 1` comes from
-          let subperiodFrom = ceiling_ $ fromIntegral offsetFrom * (numSubperiodFrom + 1)
-              subperiodTo = ceiling_ $ fromIntegral offsetFrom * (numSubperiodTo + 1)
-          newestOffset <- choose (subperiodFrom + 1, subperiodTo)
-          numOffsets <- chooseInt (1, 2)
-          offsets <- fmap (fmap Offset) . vectorOf numOffsets $ choose (newestOffset, subperiodTo)
-          pure (NewestOffset newestOffset, offsets)
+generateOffsets :: NumSubperiods a => Offset -> (a, a) -> Gen (NewestOffset, [Offset])
+generateOffsets (Offset offsetFrom) (numSubperiodFrom, numSubperiodTo) = do
+  -- note: both offsets are shifted relative to `offsetFrom` in order not to start from `now`,
+  -- that's where the extra `+ 1` comes from
+  let subperiodFrom = ceiling_ $ fromIntegral offsetFrom * (numSubperiodFrom + 1)
+      subperiodTo = ceiling_ $ fromIntegral offsetFrom * (numSubperiodTo + 1)
+  newestOffset <- choose (subperiodFrom + 1, subperiodTo)
+  numOffsets <- chooseInt (1, 2)
+  offsets <- fmap (fmap Offset) . vectorOf numOffsets $ choose (newestOffset, subperiodTo)
+  pure (NewestOffset newestOffset, offsets)
 
 -- |Generates an arbitrary input for `cleanup` consisting of multiple,
 -- |properly-aligned periods, and the `times` and `newestTimes` inside all
@@ -140,29 +138,42 @@ arbitraryMultiPeriodBaseTestData :: Gen (BaseTestData [] Int)
 arbitraryMultiPeriodBaseTestData = arbitraryNow $ \now -> do
   numPeriods <- chooseInt (1, 4)
   offsetFrom <- Offset . hours <$> chooseInt (1, 10)
-  periods <- unfoldrM nextPeriod (offsetFrom, numPeriods)
-  pure (periods, Sorted [], Sorted [])
+  infos <- unfoldrM nextPeriod (offsetFrom, numPeriods)
+  --let (periods, times, newestTimes) = sequence $ (\(info, times, newestTimes) -> (info, getSorted times, getSorted newestTimes)) <$> infos
+  let periods = (\(x, _, _) -> x) <$> infos
+      offsets = concatMap (\(_, x, _) -> x) infos
+      newestOffsets = concatMap (\(_, _, x) -> x) infos
+
+  let times = flip addLocalTime now . secondsToNominalDiffTime . intToSeconds . negate <$> coerce offsets
+      newestTimes = flip addLocalTime now . secondsToNominalDiffTime . intToSeconds . negate <$> coerce newestOffsets
+
+  pure (periods, Sorted $ sort times, Sorted $ sort newestTimes)
 
   where
-    nextPeriod :: (Offset, Int) -> Gen (Maybe (PeriodInfo Int, (Offset, Int)))
+    nextPeriod :: (Offset, Int) -> Gen (Maybe ((PeriodInfo Int, [NewestOffset], [Offset]), (Offset, Int)))
     nextPeriod (offsetFrom, numPeriods) =
       if numPeriods == 0
       then pure Nothing
       else do
-        (offsetTo, period) <- generatePeriod offsetFrom
+        (offsetTo, info) <- generatePeriod offsetFrom
         let nextOffsetFrom = offsetTo
-        pure . Just $ (period, (nextOffsetFrom, numPeriods - 1))
+        pure . Just $ (info, (nextOffsetFrom, numPeriods - 1))
 
-    generatePeriod :: Offset -> Gen (Offset, PeriodInfo Int)
-    generatePeriod (Offset offsetFrom) = do
+    generatePeriod :: Offset -> Gen (Offset, (PeriodInfo Int, [NewestOffset], [Offset]))
+    generatePeriod oFrom@(Offset offsetFrom) = do
       numSubperiods <- chooseInt (1, 5)
+      generatedOffsets <- traverse (generateOffsets oFrom) . adjacentPairs $ zeroList numSubperiods
+      let (newestOffsets, offsets) = sequence $ (\(newestTime, times) -> ([newestTime], times)) <$> generatedOffsets
       let offsetTo = offsetFrom * (numSubperiods + 1)
       pure
         ( Offset offsetTo
-        , ( ( PrettyTimeInterval . secondsToNominalDiffTime . intToSeconds $ offsetFrom
-            , PrettyTimeInterval . secondsToNominalDiffTime . intToSeconds $ offsetTo
+        , ( ( ( PrettyTimeInterval . secondsToNominalDiffTime . intToSeconds $ offsetFrom
+              , PrettyTimeInterval . secondsToNominalDiffTime . intToSeconds $ offsetTo
+              )
+            , numSubperiods
             )
-          , numSubperiods
+          , newestOffsets
+          , concat offsets ++ coerce newestOffsets
           )
         )
 
