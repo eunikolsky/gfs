eddc7c705cefdb791f38b60623a4757048a219be
Eugene <e@egeek.me>
2023-01-07 16:32:23 +0200

    Test that all times older than a fixed offset are removed

--- src/GFS.hs
+++ src/GFS.hs
@@ -2,11 +2,16 @@ module GFS
   ( gfsRemove
   ) where
 
+import Data.Time.Clock
 import Data.Time.LocalTime
 
 -- TODO use `newtype`
 type Now = LocalTime
 
+-- TODO use `newtype`
+type Offset = NominalDiffTime
+
 -- TODO use `Sorted LocalTime`?
-gfsRemove :: Now -> [LocalTime] -> [LocalTime]
-gfsRemove _ _ = []
+gfsRemove :: Now -> Offset -> [LocalTime] -> [LocalTime]
+gfsRemove _ _ [] = []
+gfsRemove _ _ times = times
--- test/GFSSpec.hs
+++ test/GFSSpec.hs
@@ -3,6 +3,7 @@ module GFSSpec where
 import GFS
 
 import Data.Time.Calendar.OrdinalDate
+import Data.Time.Clock
 import Data.Time.LocalTime
 import Test.Hspec
 import Test.QuickCheck
@@ -11,8 +12,18 @@ spec :: Spec
 spec = do
   describe "gfsRemove" $ do
     it "returns nothing for empty times" $
-      property $ \(ALocalTime now) ->
-        gfsRemove now [] == []
+      property $ \(ALocalTime now) (AOffset offset) ->
+        gfsRemove now offset [] == []
+
+    describe "given fixed 1 hour offset" $ do
+      let offset = 60 * 60 :: NominalDiffTime
+
+      it "returns all times older than 1 hour" $
+        property $ \(ALocalTime now) -> do
+          -- all times are older than 1 hour
+          -- TODO extract the generator?
+          times <- fmap (flip addLocalTime now . negate . (+ offset) . fromInteger) <$> listOf (chooseInteger (0, 9000))
+          pure . counterexample ("times: " ++ show times) $ gfsRemove now offset times == times
 
 -- | Newtype wrapper for `LocalTime` in order to implement the `Arbitrary` instance.
 newtype ALocalTime = ALocalTime LocalTime
@@ -25,3 +36,9 @@ instance Arbitrary ALocalTime where
     -- TODO use random second
     time <- TimeOfDay <$> chooseInt (0, 23) <*> chooseInt (0, 59) <*> pure 0
     pure . ALocalTime $ LocalTime day time
+
+newtype AOffset = AOffset NominalDiffTime
+  deriving Show
+
+instance Arbitrary AOffset where
+  arbitrary = AOffset . fromInteger <$> chooseInteger (0, 86400)
