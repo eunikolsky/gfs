3ca255602c53ddda677e15d23c8fada02f961b0e
Eugene <e@egeek.me>
2023-01-08 20:08:43 +0200

    Use `TimeList` to ensure always sorted times

--- src/GFS.hs
+++ src/GFS.hs
@@ -1,7 +1,9 @@
 module GFS
   ( gfsRemove
+  , mkTimeList
   ) where
 
+import Data.List (sort, uncons)
 import Data.Time.Clock
 import Data.Time.LocalTime
 
@@ -11,14 +13,25 @@ type Now = LocalTime
 -- TODO use `newtype`
 type Offset = NominalDiffTime
 
--- TODO use more specific `LocalTimeList`? unique, sorted times!
-gfsRemove :: Now -> Offset -> [LocalTime] -> [LocalTime]
-gfsRemove now offset times =
-  -- "remove" means to return times from the function
+gfsRemove :: Now -> Offset -> TimeList -> TimeList
+gfsRemove now offset (TimeList times) =
+  -- "to remove" means "to return times from this function"
   let removeBeforeTime = addLocalTime (negate offset) now
       (tooOld, newerThanOffset) = span (< removeBeforeTime) times
-      -- FIXME partial function!
       -- TODO if the input is always sorted, only one pass through `times` is always enough
-      oldestInNewerThanOffset = minimum newerThanOffset
-  -- TODO what about duplicates? `/= oldest` would remove multiple values
-  in tooOld ++ filter (/= oldestInNewerThanOffset) newerThanOffset
+      unnecessaryNewerThanOffset = case uncons newerThanOffset of
+        Just (_oldestInNewer, xs) -> xs
+        Nothing -> []
+  -- TODO should we require uniqueness of time values?
+  -- TODO concatenating the filtered sorted times in correct order always produces a sorted
+  -- list — is it possible to explain this to the type system?
+  in mkTimeList $ tooOld ++ unnecessaryNewerThanOffset
+
+-- TODO extract to a new module?
+-- | Sorted (oldest to newest) list of `LocalTime` values.
+newtype TimeList = TimeList { unTimeList :: [LocalTime] }
+  deriving (Eq, Show)
+
+-- | Smart constructor for `TimeList` — sorts the input list if necessary.
+mkTimeList :: [LocalTime] -> TimeList
+mkTimeList = TimeList . sort
--- test/GFSSpec.hs
+++ test/GFSSpec.hs
@@ -2,6 +2,7 @@ module GFSSpec where
 
 import GFS
 
+import Data.List (singleton)
 import Data.Time.Calendar.OrdinalDate
 import Data.Time.Clock
 import Data.Time.LocalTime
@@ -13,7 +14,8 @@ spec = do
   describe "gfsRemove" $ do
     it "returns nothing for empty times" $
       property $ \(ALocalTime now) (AOffset offset) ->
-        gfsRemove now offset [] == []
+        let noTimes = mkTimeList []
+        in gfsRemove now offset noTimes == noTimes
 
     describe "given fixed 1 hour offset" $ do
       let offset' = 60 * 60 :: Integer
@@ -23,19 +25,19 @@ spec = do
         property $ \(ALocalTime now) -> do
           -- all times are older than 1 hour
           -- TODO extract the generator?
-          times <- fmap (flip addLocalTime now . negate . (+ offset) . fromInteger) <$> listOf (chooseInteger (1, 9000))
+          times <- mkTimeList . fmap (flip addLocalTime now . negate . (+ offset) . fromInteger) <$> listOf (chooseInteger (1, 9000))
           let cleaned = gfsRemove now offset times
           pure . counterexample ("times: " <> show times <> "\ncleaned: " <> show cleaned) $ cleaned == times
 
       it "returns nothing for one time newer than 1 hour" $
         property $ \(ALocalTime now) -> do
-          time <- flip addLocalTime now . negate . fromInteger <$> chooseInteger (1, offset' - 1)
-          pure . counterexample ("time: " <> show time) $ gfsRemove now offset [time] == []
+          oneTime <- mkTimeList . singleton . flip addLocalTime now . negate . fromInteger <$> chooseInteger (1, offset' - 1)
+          pure . counterexample ("oneTime: " <> show oneTime) $ gfsRemove now offset oneTime == mkTimeList []
 
       it "returns nothing for (keeps) time exactly 1 hour older" $
-        property $ \(ALocalTime now) -> do
-          let time = addLocalTime (negate offset) now
-          gfsRemove now offset [time] == []
+        property $ \(ALocalTime now) ->
+          let oneTime = mkTimeList . singleton $ addLocalTime (negate offset) now
+          in gfsRemove now offset oneTime == mkTimeList []
 
       it "returns all times except oldest" $
         property $ \(ALocalTime now) -> do
@@ -43,9 +45,9 @@ spec = do
           let oldestTime = addLocalTime (negate . fromInteger $ oldestTimeOffset) now
           -- TODO is it possible to encapsulate and hide the offset subtractions so that they are always positive in the properties?
           times <- fmap (flip addLocalTime now . negate . fromInteger) <$> listOf (chooseInteger (1, oldestTimeOffset))
-          let inputTimes = oldestTime : times
+          let inputTimes = mkTimeList $ oldestTime : times
               cleaned = gfsRemove now offset inputTimes
-          pure . counterexample ("input times: " <> show inputTimes <> "\ncleaned: " <> show cleaned) $ cleaned == times
+          pure . counterexample ("input times: " <> show inputTimes <> "\ncleaned: " <> show cleaned) $ cleaned == mkTimeList times
 
 -- | Newtype wrapper for `LocalTime` in order to implement the `Arbitrary` instance.
 newtype ALocalTime = ALocalTime LocalTime
