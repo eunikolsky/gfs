139c81480becf8c713248674e4cdc1800d444090
Eugene <e@egeek.me>
2023-01-24 07:57:40 +0200

    Add test for empty input

--- test/InputParserSpec.hs
+++ test/InputParserSpec.hs
@@ -10,8 +10,10 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "parseTimes" $ do
+    let format = "%Y-%m-%d_%H_%M_%S"
+
     it "parses times based on format" $ do
-      let actual = parseTimes "%Y-%m-%d_%H_%M_%S"
+      let actual = parseTimes format
             [ "1999-12-31_00_00_00"
             , "2023-07-13_12_31_49"
             , "2000-01-01_23_59_59"
@@ -24,6 +26,9 @@ spec = do
             ]
 
       actual `shouldBe` expected
+
+    it "returns empty list for empty input" $
+      parseTimes format [] `shouldBe` []
 
 createTime :: (Int, Int, Int, Int, Int, Int) -> LocalTime
 createTime (y, m, d, h, mi, s) = LocalTime
